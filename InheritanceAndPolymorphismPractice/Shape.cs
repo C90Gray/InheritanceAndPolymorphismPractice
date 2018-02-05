f6cde51c7e070810cab6de89735963ba91fa6d23
C90Gray <cmusic.gray@gmail.com>
2018-02-05 11:51:28 -0500

    first commit

---
+++ InheritanceAndPolymorphismPractice/Shape.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceAndPolymorphismPractice
+{
+    public class Shape
+    {
+        private int sides;
+
+        public int Sides
+        {
+            get { return this.sides; }
+            set { this.sides = value; }
+        }
+
+        public Shape()
+        {
+
+        }
+
+        public Shape (int sides)
+        {
+            this.sides = sides;
+        }
+
+       
+    }
+}
